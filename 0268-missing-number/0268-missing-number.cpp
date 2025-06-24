15ac4291f24e774cba47ffdb89fb21f3f7d42b8a
vishal ravichandran <vishaleditz007@gmail.com>
2025-06-24 21:30:30 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,12 +4,14 @@ public:
         sort(nums.begin(),nums.end());
         int i;
         int n=nums.size();
-       int sum=n*(n+1)/2;
-       int s2=0;
-       for(i=0;i<n;i++)
-       {
-        s2+=nums[i];
-       }
-       return sum-s2;
+        int xor1=0;
+        int xor2=0;
+        for(i=0;i<n;i++)
+        {
+            xor1=xor1^(i+1);
+            xor2=xor2^nums[i];
+        }
+        
+      return xor1^xor2;
     }
 };
